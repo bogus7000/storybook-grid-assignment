a127fe9ca783fed4e5c8c80e0feebcef670f5b64
Bohdan Brodskyi <bohdan.brodskyi@uptimedevelopment.pl>
2022-11-30 23:02:15 +0100

    chore: remove boilerplate

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,9 +1,56 @@
-export const parameters = {
-  actions: { argTypesRegex: "^on[A-Z].*" },
-  controls: {
-    matchers: {
-      color: /(background|color)$/i,
-      date: /Date$/,
+import "../src/index.css";
+
+const customViewportOptions = {
+  XSM: {
+    name: "XSM",
+    styles: {
+      width: "360px",
+      height: "840px",
     },
   },
-}
+  SM: {
+    name: "SM",
+    styles: {
+      width: "600px",
+      height: "1300px",
+    },
+  },
+  MD: {
+    name: "MD",
+    styles: {
+      width: "834px",
+      height: "1480px",
+    },
+  },
+  LG: {
+    name: "LG",
+    styles: {
+      width: "1024px",
+      height: "2200px",
+    },
+  },
+  XLG: {
+    name: "XLG",
+    styles: {
+      width: "1288px",
+      height: "2500px",
+    },
+  },
+};
+
+export const parameters = {
+  backgrounds: {
+    default: "light",
+    values: [
+      {
+        name: "light",
+        value: "#f6f8f7",
+      },
+    ],
+  },
+  viewport: {
+    viewports: {
+      ...customViewportOptions,
+    },
+  },
+};
